5f33d9cb9f354c72de466af0195d132af0b2c276
Jazzy <jazmiwg@gmail.com>
2022-11-28 21:19:59 -0800

    added some routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const customerRoutes = require('./customerRoutes');
 const cartRoutes = require('./cartRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const productRoutes = require('./productRoutes');
 
 router.use('/customer', customerRoutes);
 router.use('/cart', cartRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
 
 module.exports = router;
