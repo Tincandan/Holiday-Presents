3ee180f9acd0fee6a8f83ece724fa8773eb0ba3e
BrownCAB <brown.calah@hotmail.com>
2022-11-29 18:53:02 -0800

    Testing Login and Logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logout = async () => {
     // Make a POST request to destroy the session on the back end
-    const response = await fetch('/api/customerRoutes/logout', {
+    const response = await fetch('/api/customer/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
