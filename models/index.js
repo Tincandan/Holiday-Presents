25cbfd72b75db3ffb2d5ed4e43ae4538084cdebe
Jazzy <jazmiwg@gmail.com>
2022-12-01 16:47:57 -0800

    got it to link

--- models/index.js
+++ models/index.js
@@ -3,12 +3,12 @@ const Category = require('./Category');
 const Cart = require('./Cart');
 const Product = require('./Product');
 
-/*Category.hasMany(Product, {
-    foreignKey: ''
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
 });
 
 Product.belongsTo(Category, {
-    foreignKey: ''
-}); */
+    foreignKey: 'category_id',
+});
 
 module.exports = { Customer, Category, Cart, Product };
