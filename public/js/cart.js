7f884db6d87437a5bbfae1633724edb1a78eb3d2
BrownCAB <brown.calah@hotmail.com>
2022-12-03 13:56:17 -0800

    Modified handlebars and css

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,7 +3,7 @@
 //--- Update Qty and Price ---/
 $(document).ready(function() {
   update_amounts();
-  $('qty, price').on('keyup keypress blur change',
+  $('.qty, .price').on('keyup keypress blur change',
  function(e) {
   update_amounts();
  });
@@ -11,13 +11,13 @@ $(document).ready(function() {
 
 //--- Sum of the Total Amount ---/
 function update_amounts() {
-  var sum = 0;
+  var sum = 0.0;
   $('#cartTable > tbody > tr').each(function() {
     var qty = $(this).find('.qty').val();
     var price = $(this).find('.price').val
-    var amount = (qry*price)
+    var amount = (qty*price)
     sum+=amount;
-    $(this).find('amount').text('' + amount);
+    $(this).find('.amount').text('' + amount);
   });
   $('.total').text(sum);
 }
@@ -30,7 +30,7 @@ var minusBtn = $(".cart-qty-minus");
 
 incrementQty = plusBtn.click(function(){
   var $n = $(this)
-  .parent("button-container")
+  .parent(".button-container")
   .find(".qty");
   $n.val(Number($n.val())+1);
   update_amounts();
@@ -38,7 +38,7 @@ incrementQty = plusBtn.click(function(){
 
 decrementQty = minusBtn.click(function(){
   var $n = $(this)
-  .parent("button-container")
+  .parent(".button-container")
   .find(".qty");
   var QtyVal = Number($n.val());
   if (QtyVal > 0) {
@@ -48,3 +48,15 @@ decrementQty = minusBtn.click(function(){
 });
 
 // forEach product placed into the cart generate a tbody
+
+
+
+
+// delete one product-row in tbody
+var table = document.getElementById('table');
+var row = document.getElementsByTagName('tbody')[0];
+
+
+function deleteRow () {
+    row.parentNode.removeChild(row);
+};
