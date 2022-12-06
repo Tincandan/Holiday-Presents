272439ae78e958e3c36f8fa985819a5da26f39e0
BrownCAB <brown.calah@hotmail.com>
2022-12-05 20:49:24 -0800

    updating server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
